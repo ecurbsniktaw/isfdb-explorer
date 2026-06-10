ddfcbf07624e549779e55a17fa05aeff925016f9
Bruce Watkins <bruce.watkins@gmail.com>
2026-06-10 08:41:51 -0400

    remove sample code

---
+++ mycode/import subprocess.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# Runs awk and captures its output directly into a variable
+# result = subprocess.check_output("awk '{print $1}' data.txt", shell=True, text=True)
+# print(result)
+
+
+# wc -l /var/log/nginx/access.log
+
+result = subprocess.check_output("wc -l /var/log/nginx/access.log", shell=True, text=True)
+print(f'# lines in log file = {result}')
