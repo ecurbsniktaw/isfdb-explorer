57a319fc0cb9621cca5b6fbab0e5f847581470b5
Bruce Watkins <bruce.watkins@gmail.com>
2026-06-10 08:44:55 -0400

    change file name

--- mycode/logsummary.py
+++ mycode/logsummary.py
@@ -1,8 +1,8 @@
 import subprocess
 
 # Runs awk and captures its output directly into a variable
-result = subprocess.check_output("awk '{print $1}' data.txt", shell=True, text=True)
-print(result)
+# result = subprocess.check_output("awk '{print $1}' data.txt", shell=True, text=True)
+# print(result)
 
 
 # wc -l /var/log/nginx/access.log
